6a410a14cbb996f32b738f786c2f1a390172cec5
raymond dzery hago <raymond.rakotonirainy@bubble-go.mg>
2025-04-01 15:44:51 +0300

    feat: header components

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,6 @@
+import Header from "@/components/Header/Header"
 import { Text, View } from "react-native"
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text className="text-red-500">
-        Edit app/index.tsx to edit this screen.
-      </Text>
-      <View className="w-10 h-10 bg-blue-500" />
-    </View>
-  )
+  return <Header />
 }
